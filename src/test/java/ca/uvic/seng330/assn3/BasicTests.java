c8e10f2f5e82af25f7e8be76f4d636ad249fcd8a
SethTolkamp <stolkamp@uvic.ca>
2018-11-03 14:05:10 -0700

    basic tests

--- src/test/java/ca/uvic/seng330/assn3/BasicTests.java
+++ src/test/java/ca/uvic/seng330/assn3/BasicTests.java
@@ -9,4 +9,11 @@ public class BasicTests {
     System.out.println("hi");
     //return true;
   }
+  
+  @Test
+  public void testBasicMVC() {
+    AdditionModel model = new AdditionModel();
+    AdditionController controller = new AdditionController(model);
+    AdditionView view = new AdditionView(controller, model);
+  }
 }
