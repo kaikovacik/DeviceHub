fde17122715b216c69f835a1ea97a29d3bb7bcb0
SethTolkamp <stolkamp@uvic.ca>
2018-11-03 14:28:55 -0700

    some basic tests

--- src/test/java/ca/uvic/seng330/assn3/BasicTests.java
+++ src/test/java/ca/uvic/seng330/assn3/BasicTests.java
@@ -12,8 +12,23 @@ public class BasicTests {
   
   @Test
   public void testBasicMVC() {
-    AdditionModel model = new AdditionModel();
-    AdditionController controller = new AdditionController(model);
-    AdditionView view = new AdditionView(controller, model);
+    //    AdditionModel model = new AdditionModel();
+    //    AdditionController controller = new AdditionController(model);
+    //    AdditionView view = new AdditionView(controller, model);
+
+    System.out.println("Test Basic MVC");
+
+    HubController hubC = new HubController();
+    Camera camera1 = new Camera(hubC);
+    Thermostat thermostat1 = new Thermostat(hubC);
+    Lightbulb lightbulb1 = new Lightbulb(hubC);
+    SmartPlug smartplug1 = new SmartPlug(hubC);
+
+    hubC.register(camera1);
+    hubC.register(thermostat1);
+    hubC.register(lightbulb1);
+    hubC.register(smartplug1);
+
   }
+  
 }
